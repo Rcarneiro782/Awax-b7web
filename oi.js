103986651cd8b9d30bc0cf0efc00372aaaa19c46
Rafael <rcarneiro782@gmail.com>
2021-11-27 14:52:09 -0300

    Restos dos arquivos

---
+++ oi.js
@@ -0,0 +1,8 @@
+let valo1=parseInt(gets());
+let valor2=parseInt(gets());
+let valor3=parseFloat(gets());
+
+// Escreva o seu código aqui
+let salary = parseFloat(valor2*valor3).toFixed(2);
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary);
